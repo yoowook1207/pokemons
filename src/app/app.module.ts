6d84ea4000d49b187322028bcc54d0855bc07d82
Sung Yoo <yoowook1207@gmail.com>
2022-10-21 14:18:44 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PokemonMainComponent } from './pokemon-main/pokemon-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonMainComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pokemon-main/pokemon-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-main',
+  templateUrl: './pokemon-main.component.html',
+  styleUrls: ['./pokemon-main.component.scss']
+})
+export class PokemonMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
